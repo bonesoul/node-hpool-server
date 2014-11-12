878f6afec1044b33add5488861fcbb5cc3a6fbf3
bonesoul <shalafiraistlin@gmail.com>
2014-11-12 15:21:18 +0200

    removed BOM

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-﻿// 
+// 
 //     hpool-stratum - sratum protocol module for hpool-server
 //     Copyright (C) 2013 - 2014, hpool project 
 //     http://www.hpool.org - https://github.com/int6/hpool-stratum
